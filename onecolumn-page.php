7beeae04fa843f2a4a8b0d6167783a1c5d9abd5b
Colin McCloskey <colin.mccloskey@gmail.com>
2010-12-08 17:57:05 -0500

    page template change

--- onecolumn-page.php
+++ onecolumn-page.php
@@ -20,7 +20,7 @@ get_header(); ?>
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
       <div class="page-content">
-				<h2><?php the_title(); ?></h2>
+				<h1><?php the_title(); ?></h1>
 
 				<?php the_content(); ?>
 				<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
