cb26a83b67c3dce47f5c4ec28609d04d96ae9659
Colin McCloskey <colin.mccloskey@gmail.com>
2010-12-11 00:42:02 -0500

    obnoxious facebook opengraph stuff

--- header.php
+++ header.php
@@ -9,40 +9,50 @@
  * @since YetAnotherBeerShow 0.9
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraphprotocol.org/schema/" <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 * We filter the output of wp_title() a bit -- see
-	 * twentyten_filter_wp_title() in functions.php.
-	 */
-	wp_title( '|', true, 'right' );
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
 
-	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
+<?php if (have_posts()):while(have_posts()):the_post();endwhile;endif;?>
+<!-- Facebook Opengraph -->
+  <meta property="fb:app_id" content="your_app_id" />
+  <meta property="fb:admins" content="your_admin_id" />
+  <meta property="og:url" content="<?php the_permalink() ?>"/>
+<?php if (is_single()) { ?>
+  <meta property="og:title" content="<?php single_post_title(''); ?>" />
+  <meta property="og:description" content="<?php echo strip_tags(get_the_excerpt($post->ID)); ?>" />
+  <meta property="og:type" content="article" />
+  <?php if ( in_category("video", $post) ):?>
+  <meta property="og:image" content="<?php echo extract_youtube_thumb_url($post->post_content) ?>" />
+  <?php else :?>
+  <meta property="og:image" content="<?php echo wp_get_attachment_thumb_url( get_post_thumbnail_id( $post->ID ) ) ?>" />
+  <?php endif; ?>
+<?php } else { ?>
+  <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
+  <meta property="og:description" content="<?php bloginfo('description'); ?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="<?php bloginfo('template_url') ?>/path/to-your/logo.jpg" />
+<?php } ?>
 
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
-?>
+  <title><?php wp_title( '|', true, 'right' ); ?></title>
 
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+  <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+  <!-- YABS Scripts -->
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
   <script type="text/javascript" src="<?php echo get_bloginfo('template_url') . "/js/yabs_youtube_hacks.js" ?>"></script>
   <script type="text/javascript" src="<?php echo get_bloginfo('template_url') . "/js/yabs_ticker.js" ?>"></script>
 
+<?php
+	/* Support threaded comments on comment pages: */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Allow themes and plugins to hook into the head section: */
+	wp_head();
+?>
 </head>
 
 <body <?php body_class(); ?>>
