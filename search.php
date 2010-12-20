abcd8b7a5ad74cf23ca54a56480f8862d0c589e7
Colin McCloskey <colin.mccloskey@gmail.com>
2010-12-19 23:57:27 -0500

    reformat search result pages

--- search.php
+++ search.php
@@ -9,20 +9,37 @@
 
 get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
-				<h1><?php printf( __( 'Search Results for: %s', 'twentyten' ), '' . get_search_query() . '' ); ?></h1>
-				<?php
-				/* Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-search.php and that will be used instead.
-				 */
-				 get_template_part( 'loop', 'search' );
-				?>
-<?php else : ?>
-					<h2><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
-					<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
-					<?php get_search_form(); ?>
-<?php endif; ?>
+<div id="content-wrapper" class="wrapper">
+  
+  <div id="content" class="section full-width">
+    
+    <div class="page-content">
+			<h1><?php printf( __( 'Search Results for: %s', 'twentyten' ), '' . get_search_query() . '' ); ?></h1>
+      
+      <div class="gallery">
+        <?php if ( have_posts() ) :
+  			  get_template_part( 'loop', 'search' );
+  			else : ?>
+  			  <p>
+  			    Sorry, we couldn't find anything that matched your search criteria.
+  			    Please try again, or <a href="/">start from the beginning</a>.
+  			  </p>
+				  <?php get_search_form(); ?>
+				
+  			<?php endif; ?>
+			</div>
+		</div>
+			
+	</div> <!--/content-->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<div class="clear"></div>
+</div> <!--/content-wrapper-->
+
+
+<div id="footer-wrapper" class="wrapper">
+  <div id="footer" class="section">
+    <h2 class="mast"><span>Yet Even More Stuff</span></h2>
+    <?php get_footer(); ?>
+  </div> <!--/footer-->
+</div> <!--/footerwrap-->
+
