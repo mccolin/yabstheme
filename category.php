b9bf4416bce46c55b8f35fa5b94e2a10cdfdae58
Colin McCloskey <colin.mccloskey@gmail.com>
2010-12-19 23:53:25 -0500

    general category page fix

--- category.php
+++ category.php
@@ -9,36 +9,42 @@
 
 get_header(); ?>
 
-<div id="body-wrapper">
-
-  <div id="content">
-
-				<h1><?php
-					printf( __( 'Topics / %s', 'twentyten' ), '' . single_cat_title( '', false ) . '' );
-				?></h1>
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '' . $category_description . '';
-
-				/* Run the loop for the category page to output the posts.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-category.php and that will be used instead.
-				 */
-				get_template_part( 'loop', 'category' );
-				?>
-				
+<div id="content-wrapper" class="wrapper">
+  
+  <div id="content" class="section full-width">
+    
+    <div class="page-content">
+			<h1><?php echo single_cat_title( '', false ) ?> Posts</h1>
+			<?php
+				$category_description = category_description();
+				if ( ! empty( $category_description ) )
+					echo '' . $category_description . '';
+      ?>
+      
+      
+      <div class="gallery">
+        <?php
+  			/* Run the loop for the category page to output the posts.
+  			 * If you want to overload this in a child theme then include a file
+  			 * called loop-category.php and that will be used instead.
+  			 */
+  			get_template_part( 'loop', 'category' );
+  			?>
+			</div>
+		</div>
+			
 	</div> <!--/content-->
 
-    <div id="sidebar">
-      <?php get_sidebar(); ?>
-    </div> <!--/sidebar-->
+<div class="clear"></div>
+</div> <!--/content-wrapper-->
 
-  </div> <!--/bodywrap-->
 
-  <div id="footer-wrapper">
-    <div id="footer">
-      <?php get_footer(); ?>
-    </div>
-  </div>
+<div id="footer-wrapper" class="wrapper">
+  <div id="footer" class="section">
+    <h2 class="mast"><span>Yet Even More Stuff</span></h2>
+    <?php get_footer(); ?>
+  </div> <!--/footer-->
+</div> <!--/footerwrap-->
+
+
   
